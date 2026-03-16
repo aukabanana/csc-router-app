92804a5d42eec0af99ddb33b13c6d5aa8c9606e3
ratchapol <hope25501@gmail.com>
2026-03-16 15:48:29 +0700

    feat(add): router path

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import App from './App.tsx'
+
+import AboutPage from './pages/AboutPage.tsx'
+import ContactPage from './pages/ContactPage.tsx'
+
+const router = createBrowserRouter ([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/about',
+    element: <AboutPage />
+  },
+  {
+    path: '/contact',
+    element: <ContactPage />
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
